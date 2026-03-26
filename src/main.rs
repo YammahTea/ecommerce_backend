2f4b90ca4c07179285a667099dfb9db0706e9d1e
clocky <cloudyqiqi@gmail.com>
2026-03-26 23:50:52 +0300

    Add health endpoint, update dependencies

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::Router;
+use axum::routing::get;
+
+#[tokio::main]
+async fn main() {
+    let app: Router = app();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
+}
+
+fn app () -> Router {
+    Router::new()
+        .route("/health", get(health_check))
+}
+
+async fn health_check() -> impl IntoResponse {
+    // more check ups will be added as the app develops
+    (StatusCode::OK, "Looks good").into_response()
 }
